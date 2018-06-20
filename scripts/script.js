0661d433060d61f8e0a7f55408f73f914033c0ba
Asia <maungsterr@gmail.com>
2018-06-19 21:33:04 -0700

    last last test

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,10 @@
 // Commands:
 //
 
-const pablum = ["Either or. I mean, sure, whatever.", "I'm all about this overcast day", "I lost myself inside this beautiful day.", "Everything interacts with itself and everything around it throughout time and space.", "You can quit any time."]
 
 module.exports = function(robot) {
+  const pablum = ["Either or. I mean, sure, whatever.", "I'm all about this overcast day", "I lost myself inside this beautiful day.", "Everything interacts with itself and everything around it throughout time and space.", "You can quit any time."]
+
   //  YOUR CODE HERE
   robot.hear(/purpose|life/i, function(msg) {
     return msg.send(msg.random(pablum));
